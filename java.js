98c90e1898c5b64bd3e13cdae84cf587c5e5a723
amneh_01 <amnehalhazaimeh@gmail.com>
2026-09-20 18:57:11 +0300

    test1

--- java.js
+++ java.js
@@ -1,22 +1,23 @@
 
-const name =prompt(" Enter your name");
-const age =prompt(" Enter your age");
-const gender =prompt(" Enter your gender");
+let name =prompt(" Enter your name");
+let age =prompt(" Enter your age");
+let gender =prompt(" Enter your gender");
 
+let Text= "";
 
 if( gender === "male" )
 {
-   Text = `welcom  Mr.${name}`;
+   Text = "welcom  Mr. " + name;
 }
 
 else if( gender ==="female")
 {
-   Text(`welcom  Ms.${name}`);
+   Text = "welcom  Ms. " + name;
 }
 
 else
 {
-  Text(`welcom ${name}`);
+Text = "welcom  " + name;
 }
 
 document.write(Text);
@@ -31,26 +32,39 @@ else
 }
 
 
- const order= prompt( "enter  your order : Burger , Shawarma , Zinger " );
+ let order= prompt( "enter  your order : Burger , Shawarma , Zinger " );
+ let valid=false;
  
-if( order === "Burger" )
+if(meal =="Burger" || meal =="Shawarma" || meal=="Zinger")
 {
-   alert ( "Your order is being prepared");
-}
-
-else if( order ==="Shawarma")
-{
-    alert ( "Your order is being prepared");
-}
-
-else if( order ==="Zinger")  
-{
-   alert ( "Your order is being prepared");
+    alert("Your order is being prepared")
+    valid=true;
 }
 
 else
 {
-alert("Invalid order");
+    alert("Invalid order. Please try again")
+    valid=false;
+}
+
+
+if(Age>=18 && orderIsValid){
+    console.log("Order confirmed");
+}
+else if(Age<18 ||(orderIsValid==false)){
+    alert("Order requires verification");
 
 }
 
+
+document.write("Customer Name :"+name);
+document.write("<br>");
+document.write("Age:"+age);
+document.write("<br>");
+document.write("Gender:"+gender)
+document.write("<br>");
+document.write("Order :"+order);
+document.write("<br>");
+document.write("isAvailable:"+orderIsValid);
+document.write("<br>");
+document.write("<br>");
